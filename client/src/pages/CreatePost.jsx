01a6ac591ddc4a47e7e04ffac283881d93ba6e7a
mrmayank11 <107452244+mrmayank11@users.noreply.github.com>
2023-07-05 14:02:19 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,7 +24,7 @@ const CreatePost = () => {
         if (form.prompt && form.photo) {
             setLoading(true);
             try {
-                const response = await fetch('http://localhost:8080/post/', {
+                const response = await fetch('https://dalle-e-asna.onrender.com/post/', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
