0e8df48ec0a80385b941eab5c513f9d3d616f688
mrmayank11 <107452244+mrmayank11@users.noreply.github.com>
2023-07-05 14:16:25 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -89,7 +89,7 @@ const CreatePost = () => {
                         labelName="Your Name"
                         type="text"
                         name="name"
-                        placeholder="Ex., john doe"
+                        placeholder="Ex., mayank"
                         value={form.name}
                         handleChange={handleChange}
                     />
